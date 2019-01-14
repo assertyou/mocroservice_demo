32828da7e471b29ec32ae48fdf67aebc5d76ed9e
kai <500893@qq.com>
2019-01-14 18:43:21 +0800

    Just for  test

--- microservice-api/src/main/java/org/study/entity/Dept.java
+++ microservice-api/src/main/java/org/study/entity/Dept.java
@@ -4,11 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 /*
-* 在这里新添加一行注释，用于测试git
-*
-* 再添加一行注释
-* 在添加一行注释
-* 再添加一行注释
+Just for test
 * */
 
 @Data
